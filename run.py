bed9eb8df8d7f2aa93c4fe464efc105862ab5efe
frazerk <16587706+frazerk@users.noreply.github.com>
2020-01-16 19:46:41 -0500

    Make respond_to capable of matching multiline messages.

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
 from slackbot.bot import Bot
 import logging
 import sys
+import re
 
 
 def main():
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
     bot = Bot()
+    # make @respond_to receive multi-line messages
+    bot._dispatcher.AT_MESSAGE_MATCHER = re.compile(
+        bot._dispatcher.AT_MESSAGE_MATCHER.pattern, re.S | re.M
+    )
     bot.run()
 
 if __name__ == "__main__":
