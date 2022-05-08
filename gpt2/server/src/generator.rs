4e952d19102afa4d9e1a6e47f69e8524f2a0aa92
Kyle Frazer <16587706+frazerk@users.noreply.github.com>
2022-05-08 18:58:32 -0500

    Roll some gpt2 post-processing into the gRPC server for re-use by the website

--- gpt2/server/src/generator.rs
+++ gpt2/server/src/generator.rs
@@ -1,25 +1,24 @@
 use eyre::{Result, WrapErr};
-use gpt2_proto::gpt2::GenerateRequest;
 use rust_bert::gpt2::GPT2Generator;
 use tracing::{debug, info};
 
-use crate::Receiver;
+use crate::{Message, Receiver};
 
 const MAX_SIZE: usize = 1024;
 
 // GPT2 generation loop. Enforces that only a single generation is running at once. Not async
 // (since rust-bert isn't async), and must be run in a blocking-safe task (e.g.
 // tokio::task::spawn_blocking).
-pub fn gpt2(mut rx: Receiver) -> Result<()> {
+pub(crate) fn gpt2(mut rx: Receiver) -> Result<()> {
     use rust_bert::pipelines::generation_utils::{GenerateOptions, LanguageGenerator};
 
     let generator = load_model()?;
     let tokenizer = generator.get_tokenizer();
 
-    while let Some((args, oneshot_tx)) = rx.blocking_recv() {
-        let (prompt, prompt_size) = truncate(tokenizer, &args);
+    while let Some(msg) = rx.blocking_recv() {
+        let (prompt, prompt_size) = truncate(tokenizer, &msg);
 
-        let max_length = Some((prompt_size + args.length as usize) as i64);
+        let max_length = Some((prompt_size + msg.length as usize) as i64);
 
         let mut gen = generator.generate_indices(
             Some(&[prompt]),
@@ -38,7 +37,7 @@ pub fn gpt2(mut rx: Receiver) -> Result<()> {
 
         let output = tokenizer.decode(&trimmed, true, true);
 
-        oneshot_tx.send(output).ok();
+        msg.responder.send(output).ok();
     }
 
     Ok(())
@@ -81,11 +80,11 @@ fn load_model() -> Result<GPT2Generator> {
 
 fn truncate<'a>(
     tokenizer: &rust_bert::pipelines::common::TokenizerOption,
-    args: &'a GenerateRequest,
+    msg: &'a Message,
 ) -> (&'a str, usize) {
-    let tokenized = tokenizer.tokenize_with_offsets(&args.prompt);
+    let tokenized = tokenizer.tokenize_with_offsets(&msg.prompt);
     let tok_size = tokenized.tokens.len();
-    let max_size = args.length as usize + tok_size;
+    let max_size = msg.length as usize + tok_size;
     let overflow = max_size.saturating_sub(MAX_SIZE);
 
     let offset = tokenized
@@ -99,5 +98,5 @@ fn truncate<'a>(
 
     debug!(%offset, "truncating text");
 
-    (&args.prompt[offset..], tok_size - overflow)
+    (&msg.prompt[offset..], tok_size - overflow)
 }
--- gpt2/server/src/main.rs
+++ gpt2/server/src/main.rs
@@ -1,6 +1,10 @@
 mod generator;
 
+use std::borrow::Cow;
 use std::env;
+
+use once_cell::unsync::Lazy;
+use regex::Regex;
 use tokio::{
     sync::{mpsc, oneshot},
     task,
@@ -9,10 +13,16 @@ use tonic::{transport::Server, Request, Response, Status};
 use tracing::{debug, error, info};
 
 use gpt2_proto::gpt2::gpt2_server as proto;
-use gpt2_proto::gpt2::{GenerateRequest, GeneratedText};
+use gpt2_proto::gpt2::{GenerateRequest, GeneratedText, ScriptRequest};
 
 type Responder = oneshot::Sender<String>;
-type Message = (GenerateRequest, Responder);
+
+struct Message {
+    length: u32,
+    prompt: String,
+    responder: Responder,
+}
+
 type Sender = mpsc::UnboundedSender<Message>;
 type Receiver = mpsc::UnboundedReceiver<Message>;
 
@@ -33,10 +43,16 @@ impl proto::Gpt2 for Gpt2 {
 
         info!(length=%payload.length, prompt=%payload.prompt, "new generation request");
 
-        gen_tx.send((payload, tx)).map_err(|err| {
-            error!(%err, "failed to send generation request");
-            Status::internal("failed to contact internal processing loop")
-        })?;
+        gen_tx
+            .send(Message {
+                length: payload.length,
+                prompt: payload.prompt,
+                responder: tx,
+            })
+            .map_err(|err| {
+                error!(%err, "failed to send generation request");
+                Status::internal("failed to contact internal processing loop")
+            })?;
 
         let text: String = rx.await.map_err(|err| {
             error!(%err, "failed to receive generation response");
@@ -47,6 +63,93 @@ impl proto::Gpt2 for Gpt2 {
 
         Ok(Response::new(GeneratedText { text }))
     }
+
+    async fn generate_script(
+        &self,
+        request: Request<ScriptRequest>,
+    ) -> Result<Response<GeneratedText>, Status> {
+        let gen_tx = self.generator_tx.clone();
+        let (tx, rx) = oneshot::channel::<String>();
+
+        let payload = request.into_inner();
+
+        info!(length=%payload.length, prompt=%payload.script, speaker=%payload.next_speaker, "new script request");
+
+        let prompt = format!("{}\n{}: ", payload.script, payload.next_speaker);
+
+        gen_tx
+            .send(Message {
+                length: payload.length,
+                prompt,
+                responder: tx,
+            })
+            .map_err(|err| {
+                error!(%err, "failed to send generation request");
+                Status::internal("failed to contact internal processing loop")
+            })?;
+
+        let text: String = rx.await.map_err(|err| {
+            error!(%err, "failed to receive generation response");
+            Status::internal("internal processing loop failed to reply")
+        })?;
+
+        info!(%text, "gpt2 text generated");
+
+        let raw = strip_trailing_thoughts(&text);
+        let clean = strip_incomplete_sentences(raw);
+
+        Ok(Response::new(GeneratedText {
+            text: clean.to_string(),
+        }))
+    }
+}
+
+// Remove any trailing incomplete sentences, while allowing a standalone sentence fragment
+fn strip_incomplete_sentences(input: &str) -> Cow<'_, str> {
+    // Define the "punc" named capture group, and have the whole regex match a period, exclamation
+    // point, or question mark followed by at least one other character.
+    let re = Lazy::new(|| Regex::new(r"(?P<punc>[.!?])[^.!?]+\z").unwrap());
+
+    // This will find the left-most match of the regex above (a sentence-ending punctuation mark
+    // followed by some number of non-punctuation characters), and replace it with the found mark.
+    // This will truncate incomplete sentences, but leave input that is just a sentence fragment
+    // alone (since the regex will only match punctuation).
+    re.replace(input, "$punc")
+}
+
+// Remove shrek's inner monologue. Since we're generating text by formatting slack history as a
+// script or chat-log, the gpt2 model will continue to generate more entries in this fashion. We're
+// only interested shrek's next "line". This will will find the boundaries of that line, and remove
+// whatever follows.
+fn strip_trailing_thoughts(input: &str) -> &str {
+    let re = Lazy::new(|| {
+        // Scriptlikes, e.g "DONKEY: "
+        let scriptlikes = r"(:?\b|^)[[:upper:][:punct:]&&[^:] ]{3,}:";
+
+        // More permissive script-likes, but only at line starts
+        let line_start = r"^[[:word:][:punct:]#.']{3,}:";
+
+        // Lines that only have stage direction, e.g. `[Shrek kisses Fiona]`
+        let stage_direction = r"^[(\[].*[)\]]$";
+
+        // Stop claiming that people have left the conversation.
+        let left_convo = r"has left the conversation.$";
+
+        // All patterns combined into an alternation, with multi-line mode enabled. Note that
+        // we cannot use a RegexSet here, since those don't support splitting.
+        let combined = format!(
+            "(?m){}|{}|{}|{}",
+            scriptlikes, line_start, stage_direction, left_convo
+        );
+
+        Regex::new(&combined).unwrap()
+    });
+
+    // Collect into a vector so we can log it.
+    let split: Vec<_> = re.splitn(input, 2).collect();
+    debug!(?split);
+
+    split.first().unwrap()
 }
 
 #[tokio::main]
